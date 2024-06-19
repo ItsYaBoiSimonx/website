f9a1b79ebf1144d5bdd5399072c2ac91199ca60e
Simone <simone.savarese2402@gmail.com>
2024-06-19 11:38:58 +0100

    i am literally about to die

--- script.js
+++ script.js
@@ -22,3 +22,27 @@ function changeGreeting() {
 
 // Change the greeting every 2 seconds
 setInterval(changeGreeting, 3000);
+
+
+
+// Array of greetings in different languages
+const languages = [
+    "python",
+    "javascript",
+    "c++",
+    "frontend",
+    "low-level",
+    "high-level",
+    "game hacking",
+];
+
+let currentIndex = 0; // Global index variable
+
+function switchLanguage() {
+    const greetElement = document.querySelector(".languages");
+    greetElement.textContent = languages[currentIndex];
+    currentIndex = (currentIndex + 1) % languages.length; // Increment and loop back to 0 at the end
+}
+
+// Change the greeting every 2 seconds
+setInterval(switchLanguage, 3000);
